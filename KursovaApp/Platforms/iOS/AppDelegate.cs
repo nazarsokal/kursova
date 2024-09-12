3219c65f1652d11c7bc0cf52eb4368b4ed735cf7
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2024-09-12 11:02:48 +0200

    Initial Commit

---
+++ KursovaApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace KursovaApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
