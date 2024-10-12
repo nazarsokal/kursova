d920b9647c35817cb61ae44c29a18bf64fffbf18
nazarsokal <93711302+nazarsokal@users.noreply.github.com>
2024-10-12 13:15:17 +0200

    table formating

--- KursovaApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/KursovaApp.AssemblyInfo.cs
+++ KursovaApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/KursovaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KursovaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2ef3c31fb83f7a01a8c04ec14f4dbe23cb7519df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7f44c4e8805836ab96cbb50a57e00a244b3af5a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KursovaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KursovaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
